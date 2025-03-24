be0c4266e5e1707e47caf4ee82067bd717ae9e83
Capt Nork <138743746+norkart1@users.noreply.github.com>
2025-03-24 12:24:59 +0000

    Update

---
+++ frendent/app/index.jsx
@@ -0,0 +1,25 @@
+import { Link } from "expo-router";
+import { StyleSheet, Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>Hello</Text>
+
+      <Link href="/signup">Signup Page</Link> 
+      <Link href="/login">Login Page</Link> 
+    </View> 
+
+  ); 
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  title: {
+    color: "blue",
+  },
+});
