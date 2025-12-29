2e4942e04dce0395d39cf44fbc755f7294474a8b
captain nork <138743746+norkart1@users.noreply.github.com>
2025-12-29 12:50:37 +0000

    Updated

---
+++ frendent/metro.config.js
@@ -0,0 +1,15 @@
+const { getDefaultConfig } = require('expo/metro-config');
+
+const config = getDefaultConfig(__dirname);
+
+config.server = {
+  ...config.server,
+  enhanceMiddleware: (middleware) => {
+    return (req, res, next) => {
+      res.setHeader('Access-Control-Allow-Origin', '*');
+      return middleware(req, res, next);
+    };
+  },
+};
+
+module.exports = config;
